65fc397798283ffb1b532a0bf6c8116fd2c00ea9
mathi123 <colpaert.mathias@gmail.com>
2015-06-25 16:06:22 +0200

    [IMP] Remove unused usings

--- OdooRpcWrapper/OdooAPI_Create.cs
+++ OdooRpcWrapper/OdooAPI_Create.cs
@@ -1,11 +1,8 @@
 ﻿using CookComputing.XmlRpc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OdooRpcWrapper
 {
--- OdooRpcWrapper/OdooMany2oneAttribute.cs
+++ OdooRpcWrapper/OdooMany2oneAttribute.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.Remoting.Activation;
-using System.Runtime.Remoting.Contexts;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
 
 namespace OdooRpcWrapper
 {
