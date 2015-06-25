893ee13d0aba16c8e573ab15c1c01178aae5775b
mathi123 <colpaert.mathias@gmail.com>
2015-06-25 17:23:54 +0200

    [ADD] Odoo typed classes examples

---
+++ OdooTypedClasses/Lead.cs
@@ -0,0 +1,20 @@
+﻿using OdooRpcWrapper;
+
+namespace OdooTypedClasses
+{
+    [OdooObject("crm.lead")]
+    public class Lead
+    {
+        [OdooProperty("id")]
+        public int Id { get; set; }
+
+        [OdooProperty("name")]
+        public string Name { get; set; }
+
+        [OdooProperty("type")]
+        public string Type { get; set; }
+
+        [OdooMany2one("user_id")]
+        public User SalesPerson { get; set; }
+    }
+}
